0b0f07338bdd24ba52e21c428cfa73fed36454c2
Waite-M <waite_m@163.com>
2020-07-08 01:37:05 +0800

    first commit

---
+++ zhilianjob/pipelines.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import xlrd
+import xlwt
+from xlutils import copy
+
+class ZhilianjobPipeline:
+    def open_spider(self, spider):
+        #定义excel存储的路径
+        self.path = 'D:/spider/php'
+        if not os.path.exists(self.path):
+            os.makedirs(self.path,0o777)
+    def close_spider(self, spider):
+        pass
+    def process_item(self, item, spider):
+        #文件不存在则先创建文件
+        if not os.path.exists(self.path + '/job.xls'):
+            wt = xlwt.Workbook()
+            sheet = wt.add_sheet(item['page'])
+            titlelist = ['职位','公司','地区','经验','学历','需求人数','职位信息','公司福利及信息']
+            #添加字段
+            row = len(sheet.get_rows())
+            #获取itemlist
+            listitem = self.itemlist(item)
+            #同时写入字段及职位信息
+            for i in range(len(titlelist)):
+                sheet.write(0,i,titlelist[i])
+                sheet.write(1, i,listitem[i])
+            wt.save(self.path + '/job.xls')
+            print('写入成功1')
+        else:
+            rd = xlrd.open_workbook(self.path + '/job.xls')
+            wt = copy.copy(rd)
+            #以爬取页数命名的工作表名称是否在的工作表中
+            if item['page'] not in rd.sheet_names():
+                sheet = wt.add_sheet(item['page'])
+            else:
+                sheet = wt.get_sheet(item['page'])
+            titlelist = ['职位', '公司', '地区', '经验', '学历', '薪资','需求人数', '职位信息', '公司福利及信息']
+            # 获取已有列数，基于该列往下写入
+            row = len(sheet.get_rows())
+            #获取itemlist
+            listitem = self.itemlist(item)
+            if row == 0:
+                for i in range(len(titlelist)):
+                    sheet.write(0, i, titlelist[i])
+                    sheet.write(1, i, listitem[i])
+            else:
+                for i in range(len(titlelist)):
+                    sheet.write(row, i, listitem[i])
+            wt.save(self.path + '/job.xls')
+            print('写入成功2')
+
+    # 将item字典转换为list写入
+    def  itemlist(self,item):
+        listitem = []
+        listitem.append(item['job'])
+        listitem.append(item['company'])
+        listitem.append(item['region'])
+        listitem.append(item['experience'])
+        listitem.append(item['education'])
+        listitem.append(item['Salary'])
+        listitem.append(item['number'])
+        listitem.append(item['jobmessage'])
+        listitem.append(item['companymessage'])
+        return listitem
+
+
---
+++ zhilianjob/spiders/job.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+class JobSpider(scrapy.Spider):
+    name = 'job'
+    allowed_domains = []
+    start_urls = ["https://js.51jobcdn.com/in/resource/js/2020/search/common.a8c323f7.js"]
+    citydict = {}
+    #搜索职位
+    job = 'php'
+    #地点
+    position = '广州'
+    #爬取页数
+    pages = '5'
+
+    #解析js获取城市id
+    def parse(self, response):
+        city = re.findall(r'window.area={(.*?)"}', response.text)
+        #获取列表形式的城市id
+        citylist =  str(city).replace("[\'",'').replace("\']",'').replace('"','').split(',')
+        #将citylist每个元素以字典形式存到citydict内
+        for i in citylist:
+            id = i.split(':')[0]
+            name =i.split(':')[1]
+            self.citydict[name] = id
+        #将城市id,职业，爬取页码与搜索链接拼接成成的完整url进行爬取
+        for i in range(int(self.pages)):
+            searchurl = "https://search.51job.com/list/%s,000000,0000,00,9,99,%s,2,%d.html"%(self.citydict[self.position],self.job,i+1)
+            yield scrapy.Request(searchurl,callback=self.parse_joblist,meta={'page':i+1})
+    #解析搜索页面获取详细信息url
+    def parse_joblist(self,response):
+        jobdetail = response.xpath('//*[@id="resultList"]/div/p/span/a/@href').extract()
+        for url in jobdetail:
+            yield scrapy.Request(url,callback=self.parse_detail,meta={'page':response.meta['page']})
+    #解析职业详细信息页面
+    def parse_detail(self,response):
+        item = {}
+        item['job'] = response.xpath('/html/body/div[3]/div[2]/div[2]/div/div[1]/h1/text()').extract()[0]
+        item['company'] = response.xpath('/html/body/div[3]/div[2]/div[2]/div/div[1]/p[1]/a[1]/text()').extract()[0]
+        msg = response.xpath('/html/body/div[3]/div[2]/div[2]/div/div[1]/p[2]/@title').extract()[0].split('|')
+        if len(msg) <= 3:
+            item['region'] = msg[0]
+            item['experience'] = '无'
+            item['education'] = '无'
+            item['number'] = msg[1]
+        else:
+            item['region'] = msg[0]
+            item['experience'] = msg[1]
+            item['education'] = msg[2]
+            item['number'] = msg[3]
+        item['Salary'] = response.xpath('/html/body/div[3]/div[2]/div[2]/div/div[1]/strong/text()').extract()[0]
+        item['jobmessage'] = response.xpath('/html/body/div[3]/div[2]/div[3]/div[1]/div//text()').extract()
+        item['companymessage'] =response.xpath('/html/body/div[3]/div[2]/div[3]/div[3]/div/text()').extract()
+        item['page'] = str(response.meta['page'])
+        yield item
+
+
+
